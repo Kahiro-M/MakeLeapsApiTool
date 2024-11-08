de4bcc0c4266f039c47fa625207b92573d0a7b56
Kahiro-M <0204.kahiro@gmail.com>
2024-11-08 14:59:44 +0900

    タイムゾーン指定を最初に実行

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # モジュール読み込み
 import MakeLeapsApiMod as mlmod
 from datetime import datetime, timezone, timedelta
+jst = timezone(timedelta(hours=9), 'JST')
 
 # 設定ファイルから設定情報を読み込み
 configData = mlmod.readConfigIni('MakeLeaps.ini')
@@ -11,7 +12,7 @@ for archivedClient in archivedClientList:
     print('外部ID : ' + archivedClient['client_external_id'])
     print('表示名 : ' + archivedClient['display_name'])
     datetimeStr = archivedClient['date_archived']
-    jst = timezone(timedelta(hours=9), 'JST')
     dt = datetime.strptime(datetimeStr,'%Y-%m-%dT%H:%M:%S.%f%z')
     jstDatetimeStr = dt.astimezone(jst).strftime('%Y-%m-%d %H:%M:%S')
     print('アーカイブ日 : ' + jstDatetimeStr)
+
