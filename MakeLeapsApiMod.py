9f148035688f6fb8e047fb56ff54fd2f6101786d
Kahiro-M <0204.kahiro@gmail.com>
2025-07-03 11:14:54 +0900

    請求情報取得処理

--- MakeLeapsApiMod.py
+++ MakeLeapsApiMod.py
@@ -2,8 +2,6 @@
 # MakeLeaps REST API モジュール 
 # -------------------------------------------------- #   
 
-
-
 # iniファイルから設定を読み込む
 def readConfigIni(filePath='MakeLeaps.ini'):
     import configparser
@@ -97,3 +95,209 @@ def getArchivedClient(config):
     revokeToken(token,config)
 
     return response['response']
+
+# Retry-Afterに対応した再試行処理
+def getWithRetry(url, params={}, headers={}, maxRetries=5):
+    import requests
+    import time
+    for attempt in range(maxRetries):
+        response = requests.get(url, params=params, headers=headers)
+
+        if response.status_code == 200:
+            return response.json()  # 成功したらデータを返す
+
+        elif response.status_code == 429:
+            retry_after = response.headers.get("Retry-After")
+            waitTime = int(retry_after) if retry_after and retry_after.isdigit() else 60
+            print(f"[{attempt+1}/{maxRetries}] 回試行. {waitTime} 秒後に再試行...")
+            time.sleep(waitTime)
+
+        else:
+            print(f"[{attempt+1}/{maxRetries}] 回試行が失敗。status {response.status_code}")
+            break  # その他のエラーは中断
+
+    raise Exception("Request失敗")
+
+# YYYYMMDD形式の日付文字列から['YYYY-MM-DD']を作成
+def getDateList(dateBegin,dateEnd):
+    from datetime import datetime, timedelta
+
+    # 文字列を datetime オブジェクトに変換
+    startDate = datetime.strptime(dateBegin, "%Y%m%d")
+    endDate = datetime.strptime(dateEnd, "%Y%m%d")
+
+    # 日付のリストを作成
+    dateList = []
+    currentDate = startDate
+    while currentDate <= endDate:
+        dateList.append(currentDate.strftime("%Y-%m-%d"))
+        currentDate += timedelta(days=1)
+
+    return dateList
+
+def getDatetimeJST(utcStr):
+    from datetime import datetime
+    from zoneinfo import ZoneInfo
+
+    # 複数の可能な形式を順に試す
+    parse_formats = [
+        "%Y-%m-%dT%H:%M:%S.%fZ",  # 小数秒あり
+        "%Y-%m-%dT%H:%M:%SZ"      # 小数秒なし
+    ]
+
+    for fmt in parse_formats:
+        try:
+            dtUtc = datetime.strptime(utcStr, fmt)
+            dtUtc = dtUtc.replace(tzinfo=ZoneInfo("UTC"))
+            break
+        except ValueError:
+            continue
+    else:
+        raise ValueError(f"Unsupported datetime format: {utcStr}")
+
+    # 日本時間に変換し、整形
+    dtJst = dtUtc.astimezone(ZoneInfo("Asia/Tokyo"))
+    return dtJst.strftime("%Y-%m-%d %H:%M:%S")
+
+
+# 書類参照リンクのクリックされた情報を取得
+def getDocumentClickedDate(config,dateBegin,dateEnd,mode='simple'):
+    import math
+    # csvデータ準備
+    csvHeader = [
+        '会員番号',
+        '取引先名',
+        '案件名',
+        '文書送信先',
+        '文書作成日時',
+        '文書送信日時',
+        '文書参照日時',
+        '文書有効期限',
+    ]
+    csvData = [csvHeader]
+
+    # mode判定
+    modeList = ['0','simple','1','detail']
+    if mode.lower() in (m.lower() for m in modeList):
+        if(mode == '0'):
+            mode = 'simple'
+        elif(mode == '1'):
+            mode = 'detail'
+        else:
+            mode = mode.lower()
+    else:
+        # 合致が無い場合は'simple'固定
+        mode = 'simple'
+
+    # token取得
+    token = getToken(config)
+    userMakeleapsId = config['user_makeleaps_id']
+    headers = {
+        'Authorization': 'Bearer '+token,
+        'Content-Type': 'application/json',
+        'X-Api-Version': 'v2023.06.26'
+    }
+
+    # 文書検索条件の初期化
+    nullParams = {}
+
+    # 件数カウント
+    docCount = 0
+
+    # 指定の日付2つから日付リストを取得
+    dateList = getDateList(dateBegin,dateEnd)
+    for targetDate in dateList:
+        # 文書検索条件は指定の作成日、請求書、有効、1ページ100件まで
+        docParams = {
+            'date': targetDate,
+            'document_type': 'invoice',
+            'cancelled': False,
+            'per_page':100,
+        }
+        print(f'請求書作成日:{targetDate}')
+
+        # 請求書一覧　URLを指定してcurl経由でAPIを叩く
+        dockListUrl = 'https://api.makeleaps.com/api/partner/'+userMakeleapsId+'/document/'
+        nextUrl = dockListUrl
+        
+        printProgressFlg = True
+        firstGetDocListFlg = True
+        
+        # 請求書一覧の{'meta':{'next':'https://api.makeleaps.com/api.....'}}がNoneになるまで
+        while nextUrl:
+            # 請求書一覧取得
+            if(firstGetDocListFlg):
+                docListRes = getWithRetry(nextUrl, docParams, headers)
+                firstGetDocListFlg = False
+            else:
+                docListRes = getWithRetry(nextUrl, nullParams, headers)
+            nextUrl = docListRes['meta']['next']
+
+            if(printProgressFlg):
+                print(f"    対象件数:{docListRes['meta']['count']}")
+                print(f"    対象ページ数:{math.ceil(docListRes['meta']['count']/100)}")
+                printProgressFlg = False
+
+            if(docCount%100 == 0):
+                print(f"        進捗:{docCount}件 取得済み")
+
+            # 請求書一覧の現在のページ
+            for docInfo in docListRes['response']:
+                # 書き込み用データ初期化
+                clinetCode = ''
+                clinetName = ''
+                docName = ''
+                docSendTo = ''
+                docCreated = ''
+                docSent = ''
+                docCliced = ''
+                docExpiration = ''
+
+                # 文書情報
+                docMid = docInfo['mid']
+                docCreated = docInfo['date']
+                docSent = getDatetimeJST(docInfo['date_sent'])
+                docName = docInfo['project_name']
+                clinetName = docInfo['recipient_name']
+                
+                if(mode == 'detail'):
+                    # 取引先情報
+                    clientInfoUrl = docInfo['client']
+                    clientInfoRes = getWithRetry(clientInfoUrl, nullParams, headers)
+                    clinetCode = clientInfoRes['response']['client_external_id']
+                    clinetName = clientInfoRes['response']['display_name']
+
+                    # 請求書リンク情報 
+                    docLinkInfoUrl = 'https://api.makeleaps.com/api/partner/'+userMakeleapsId+'/document/'+docMid+'/pickup-link/'
+                    try:
+                        docLinkInfoRes = getWithRetry(docLinkInfoUrl, nullParams, headers)
+                    except Exception as e:
+                        print("Error:", e)
+
+                    docLinkInfo = docLinkInfoRes['response'][0]
+
+                    docSendTo = docLinkInfo['email']
+                    if(docLinkInfo['date_clicked'] == None):
+                        docCliced = ''
+                    else:
+                        docCliced = getDatetimeJST(docLinkInfo['date_clicked'])
+                    docExpiration = getDatetimeJST(docLinkInfo['expiration_date'])
+
+                # データ書き込み
+                appendData = [
+                    clinetCode,
+                    clinetName,
+                    docName,
+                    docSendTo,
+                    docCreated,
+                    docSent,
+                    docCliced,
+                    docExpiration,
+                ]
+                docCount += 1
+                csvData.append(appendData)
+
+    # token破棄
+    revokeToken(token,config)
+
+    return csvData
--- main.py
+++ main.py
@@ -1,18 +1,48 @@
 # モジュール読み込み
 import MakeLeapsApiMod as mlmod
 from datetime import datetime, timezone, timedelta
+import csv
+
 jst = timezone(timedelta(hours=9), 'JST')
 
 # 設定ファイルから設定情報を読み込み
 configData = mlmod.readConfigIni('MakeLeaps.ini')
 
-# アーカイブした取引先の表示
-archivedClientList = mlmod.getArchivedClient(configData)
-for archivedClient in archivedClientList:
-    print('外部ID : ' + archivedClient['client_external_id'])
-    print('表示名 : ' + archivedClient['display_name'])
-    datetimeStr = archivedClient['date_archived']
-    dt = datetime.strptime(datetimeStr,'%Y-%m-%dT%H:%M:%S.%f%z')
-    jstDatetimeStr = dt.astimezone(jst).strftime('%Y-%m-%d %H:%M:%S')
-    print('アーカイブ日 : ' + jstDatetimeStr)
+print('====== MakeLeaps 請求書情報取得 ======')
+print('v.1.0.0')
+
+print('------ 請求書発行日 範囲指定 ------')
+dateInBegin = input('いつから(YYYYMMDD) :')
+dateInEnd = input('いつまで(YYYYMMDD) :')
+mode = input('モード(0:simple,1:detail) :')
+
+if(int(dateInEnd)>int(dateInEnd)):
+    dateBegin = dateInEnd
+    dateEnd = dateInBegin
+else:
+    dateBegin = dateInBegin
+    dateEnd = dateInEnd
+
+print(f'請求書発行日指定:{dateBegin} - {dateEnd}')
+
+# 書類参照リンクのクリックされた情報を取得
+print(f'------ 請求書情報取得 開始 {datetime.now()} ------')
+documentClickedDateList = mlmod.getDocumentClickedDate(configData,dateBegin,dateEnd,mode)
+print(f'------ 請求書情報取得 終了 {datetime.now()} ------')
+
+# 書き出し
+with open('invoice_result.csv', 'w', newline='', encoding='utf-8-sig') as f:
+    writer = csv.writer(f)
+    writer.writerows(documentClickedDateList)
+
+
+# # アーカイブした取引先の表示
+# archivedClientList = mlmod.getArchivedClient(configData)
+# for archivedClient in archivedClientList:
+#     print('外部ID : ' + archivedClient['client_external_id'])
+#     print('表示名 : ' + archivedClient['display_name'])
+#     datetimeStr = archivedClient['date_archived']
+#     dt = datetime.strptime(datetimeStr,'%Y-%m-%dT%H:%M:%S.%f%z')
+#     jstDatetimeStr = dt.astimezone(jst).strftime('%Y-%m-%d %H:%M:%S')
+#     print('アーカイブ日 : ' + jstDatetimeStr)
 
